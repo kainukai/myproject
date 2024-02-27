7e1759b64a4d2f34770bfd988557526884be06c4
n2kasa01@students.oamk.fi <n2kasa01@students.oamk.fi>
2024-02-27 13:12:54 +0200

    First commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View } from "react-native";
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import styles from "./style/style"
+import Gameboard from "./components/Gameboard";
+
+export default function App() {
+  return(
+    <View style={styles.container}>
+      <Header />
+      <Gameboard/>
+      <Footer />
+    </View>
+  )
+}
